c48acf0efbe412a88c5cf39ec534d1fa75a3503e
Kelvin Silva <kelvin.silva@gmail.com>
2024-12-07 13:19:55 -0500

    Change to Keras and update to lastest version python

---
+++ Univariate -LSTM/util.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings(action="ignore")
+
+import yfinance as yf
+import pandas as pd
+
+def getData(ticker, start_date, end_date):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    return data
+
+#df = getData(['ORCL'], '2020-01-01', '2024-12-01')
+#print(df.head())
